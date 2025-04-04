7df267ef99a1f0b064b7d55d10a771e2f2bdcfd7
3xp0nential <3xp0.33@gmail.com>
2025-04-03 22:34:20 -0400

    added quiz buttons and prize buttons to /tomo command

--- tomodachi.lua
+++ tomodachi.lua
@@ -79,17 +79,14 @@ function tomodachi:mii1()
 end
 tomodachi.miiaccept = tomodachi.mii1
 tomodachi.miiyes = tomodachi.mii1
-
 function tomodachi:mii2()
 	citrainput:touch(.75,.55)
 end
 tomodachi.miidecline = tomodachi.mii2
 tomodachi.miino = tomodachi.mii2
-
 function tomodachi:mii3()
 	citrainput:touch(.75,.69)
 end
-
 function tomodachi:mii4()
 	citrainput:touch(.75,.80)
 end
@@ -97,6 +94,32 @@ end
 -- just the start button when playing catch because that game is awesome and deserves its own command
 function tomodachi:catchstart()
 	citrainput:touch(.5,.8)
+end
+
+-- answer buttons for zoom quiz and probably other quizzes. quiz2 copies the center func as it's the same
+function tomodachi:quiz1()
+	citrainput:touch(.5,.25)
+end
+tomodachi.quiz2 = tomodachi.center
+function tomodachi:quiz3()
+	citrainput:touch(.5,.75)
+end
+function tomodachi:quiztrue()
+	citrainput:touch(.25,.50)
+end
+function tomodachi:quizfalse()
+	citrainput:touch(.75,.50)
+end
+
+-- prizes after winning a game
+function tomodachi:prizesmall()
+	citrainput:touch(.25,.75)
+end
+function tomodachi:prizemid()
+	citrainput:touch(.5,.75)
+end
+function tomodachi:prizebig()
+	citrainput:touch(.8,.75)
 end
 
 return tomodachi
